c0fbf13d3d30a40ab242c18c308e8dfc02793651
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2026-02-26 07:57:19 -0500

    Update monitored services in config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ ALERT_EMAIL = "admin@example.com"
 CHECKS = {
     "disk_usage": {"threshold": 85},
     "cpu_usage": {"threshold": 75},
-    "services": ["apache2", "nightwatch-backup.timer"]
+    "services": ["apache2", "fail2ban", "ssh"]
 }
